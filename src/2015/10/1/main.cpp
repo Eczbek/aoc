6661c6a72a0890de51c28ca5c1263db610a09d06
Eczbek <eczbek.void@gmail.com>
2023-12-21 19:23:35 -0500

    2015-12-10

---
+++ src/2015/10/1/main.cpp
@@ -0,0 +1,25 @@
+#include <cstddef>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string input;
+	std::cin >> input;
+	for (int i = 0; i < 40; ++i) {
+		char current = input[0];
+		int count = 1;
+		std::string result;
+		for (std::size_t i = 1; i < input.size(); ++i) {
+			if (input[i] == current) {
+				++count;
+			} else {
+				result += std::to_string(count) + current;
+				current = input[i];
+				count = 1;
+			}
+		}
+		result += std::to_string(count) + current;
+		input = result;
+	}
+	std::cout << input.size() << '\n';
+}
